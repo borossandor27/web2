76b1c61c50a98a7192a50b0689d4f2347c5c9f0a
borossandor27 <boros.sandor@dszcberegszaszi.hu>
2023-04-15 21:29:46 +0200

    3.31

--- 2023-04-14/fetchapi.js
+++ 2023-04-14/fetchapi.js
@@ -4,19 +4,20 @@ const darabInput = document.getElementById("darab");
 const fizetendoInput = document.getElementById("fizetendo");
 const idHidden = document.getElementById("id");
 const kartyakDiv = document.getElementById("kartyak");
+const lapokLista = document.getElementById("lapozas");
 var maxRendelesSzam = 0;
 var actPageNum = 1;
 
 const createButton = document.getElementById("create");
-createButton.addEventListener("click", createRendeles);
+createButton.addEventListener("click", createNewRendelesItem);
 const readButton = document.getElementById("read");
 readButton.addEventListener("click", readAllRendeles);
 const updateButton = document.getElementById("update");
 readButton.addEventListener("click", updateRendeles);
 
-function createRendeles() {
+function createNewRendelesItem() {
   let url = "https://retoolapi.dev/tFpb3p/data"; //-- POST
-  const data = getDataJSON();
+  const data = getDataJSON("insert");
 
   fetch(url, {
     method: "POST", // or 'PUT'
@@ -34,27 +35,24 @@ function createRendeles() {
     });
 }
 
-function getDataJSON() {
-  //-- inserthez készült
-  let company = companyInput.value;
-  let product = productInput.value;
-  let darab = darabInput.value;
-  let fizetendo = fizetendoInput.value;
-  let rendeles = `{"name":"${company}","product":"${product}","darab":"${darab}","fizetendo": "${fizetendo}"}`;
-  return JSON.parse(rendeles);
-}
-function getDataJSON_to_modify() {
-  //-- inserthez készült
+function getDataJSON(params) {
   let company = companyInput.value;
   let product = productInput.value;
   let darab = darabInput.value;
   let fizetendo = fizetendoInput.value;
   let id = idHidden.value;
-  let rendeles = `{"id":"${id}","name":"${company}","product":"${product}","darab":"${darab}","fizetendo": "${fizetendo}"}`;
-  return JSON.parse(rendeles);
+  let rendelesItem = "";
+  if (params === "insert") {
+    rendelesItem = `{"name":"${company}","product":"${product}","darab":"${darab}","fizetendo": "${fizetendo}"}`;
+  } else {
+    rendelesItem = `{"id":"${id}","name":"${company}","product":"${product}","darab":"${darab}","fizetendo": "${fizetendo}"}`;
+  }
+
+  console.log(rendelesItem);
+  return JSON.parse(rendelesItem);
 }
 
-function readAllRendeles() {
+async function readAllRendeles() {
   removeAllChild(kartyakDiv);
   let url = "https://retoolapi.dev/tFpb3p/data";
   fetch(url)
@@ -62,6 +60,7 @@ function readAllRendeles() {
     .then((data) => {
       console.log(data.length);
       maxRendelesSzam = data.length;
+      lapozasUpdate();
       showPageRendeles(data);
     });
 }
@@ -75,31 +74,39 @@ function showPageRendeles(data) {
   for (let index = tol; index <= ig; index++) {
     const element = data[index];
     let rendeles = document.createElement("div");
+    rendeles.setAttribute("class", "card m-2 p-0");
     rendeles.innerHTML = rendelesToHTML(element);
     kartyakDiv.appendChild(rendeles);
   }
 }
+
 function rendelesToHTML(element) {
-  let text = `<div class="card m-2" >
-                    <div class="card-header">
+  let text = `<div class="card-header">
                         <h5>${element.name}</h5>
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">${element.product} (${element.darab})</h5>
-                        <h5>${element.fizetendo} HUF</h5>
+                        <h5 class="card-title">${element.product} <span>(${
+    element.darab
+  } db)</span></h5>
+                        <h5>${element.fizetendo.toLocaleString(
+                          "hu-HU"
+                        )} HUF</h5>
                     </div>
-                    <button class="btn btn-secondary" name="kivalaszt" id="${element.id}" onclick="betolt(this)">Kijelölés</button>
-                </div>`;
+                    <button class="btn btn-secondary m-1 mb-2" name="kivalaszt" id="${
+                      element.id
+                    }" onclick="rendelesAdatFromDatabase(this)">Kijelölés</button>
+                    `;
   return text;
 }
 
-function betolt(params) {
-  beviteli_mezok_torlese();
+function rendelesAdatFromDatabase(params) {
+  beviteli_mezok_kiuritese();
   let url = "https://retoolapi.dev/tFpb3p/data/" + params.id;
   fetch(url)
     .then((response) => response.json())
     .then((data) => inputMezokFeltoltese(data));
 }
+
 function inputMezokFeltoltese(data) {
   companyInput.value = data["name"];
   darabInput.value = data["darab"];
@@ -107,12 +114,14 @@ function inputMezokFeltoltese(data) {
   fizetendoInput.value = data["fizetendo"];
   idHidden.value = data["id"];
   removeAllChild(kartyakDiv);
+  removeAllChild(lapokLista);
 }
 
-function beviteli_mezok_torlese() {
+function beviteli_mezok_kiuritese() {
   companyInput.innerHTML = "";
   darabInput.innerHTML = "";
   fizetendoInput.innerHTML = "";
+  idHidden.value = -1;
 }
 
 function removeAllChild(parent) {
@@ -121,18 +130,18 @@ function removeAllChild(parent) {
   }
 }
 
-function updateRendeles() {
-  let data = getDataJSON_to_modify();
-  console.log(data);
+async function updateRendeles() {
+  let data = getDataJSON();
   let url = "https://retoolapi.dev/tFpb3p/data/" + data.id;
   // Example POST method implementation:
   putData(url, data).then((data) => {
     console.log(data); // JSON data parsed by `data.json()` call
   });
+  beviteli_mezok_kiuritese();
+  readAllRendeles();
 }
-async function putData(url = "", data = {}) {
-  console.log(data);
 
+async function putData(url = "", data = {}) {
   // JSON data parsed by `data.json()` call
   // Default options are marked with *
   const response = await fetch(url, {
@@ -149,4 +158,14 @@ async function putData(url = "", data = {}) {
     body: JSON.stringify(data), // body data type must match "Content-Type" header
   });
   return response.json(); // parses JSON response into native JavaScript objects
+}
+
+function lapozasUpdate() {
+  removeAllChild(lapokLista);
+  for (let index = 0; index < maxRendelesSzam / 10; index++) {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(index));
+    li.setAttribute("class", "page-item");
+    lapokLista.appendChild(li);
+  }
 }
